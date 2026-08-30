246c3c7cb6f7102840d1e7df89dcd13d92a4797a
jKalfis <jorgelima.kalfis@gmail.com>
2026-08-30 17:41:25 +0100

    Update gossipquest.lua
    Removed blizzard's parchment skin from quest givers
    
    Changed background to black to mach pfUI
    
    Changed all text to white
    
    -Still working on questlog

--- skins/blizzard/gossipquest.lua
+++ skins/blizzard/gossipquest.lua
@@ -12,7 +12,87 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
     SkinButton(button)
   end
 
-  do -- quest gossip
+  local function SetWhite(text)
+    if text then
+      text:SetTextColor(1, 1, 1, 1)
+      text:SetShadowColor(0, 0, 0, 1)
+      text:SetShadowOffset(1, -1)
+    end
+  end
+
+  local function SetButtonWhite(button)
+    if not button then return end
+
+    local text = button:GetFontString()
+
+    if text then
+      SetWhite(text)
+    end
+
+    if button.normalText then
+      SetWhite(button.normalText)
+    end
+
+    if button.highlightText then
+      SetWhite(button.highlightText)
+    end
+
+    if button.disabledText then
+      SetWhite(button.disabledText)
+    end
+
+    local regions = {button:GetRegions()}
+
+    for _, region in pairs(regions) do
+      if region and region:GetObjectType() == "FontString" then
+        SetWhite(region)
+      end
+    end
+  end
+
+  local function SetGossipButtonsWhite()
+    for i = 1, 32 do
+      SetButtonWhite(_G["GossipTitleButton"..i])
+      SetButtonWhite(_G["QuestTitleButton"..i])
+    end
+  end
+
+  local function SetQuestTextWhite()
+    local texts = {
+      QuestTitleText,
+      QuestDescriptionText,
+      QuestProgressTitleText,
+      QuestProgressText,
+      QuestProgressRequiredItemsText,
+      QuestProgressRequiredMoneyText,
+      QuestRewardTitleText,
+      QuestRewardText,
+      QuestRewardRewardTitleText,
+      QuestRewardItemChooseText,
+      QuestRewardItemReceiveText,
+      QuestRewardSpellLearnText,
+      QuestDetailItemReceiveText,
+      QuestDetailSpellLearnText,
+      GreetingText,
+      GossipGreetingText,
+      CurrentQuestsText,
+      AvailableQuestsText,
+      QuestObjectiveText,
+      QuestObjectivesText,
+      QuestObjectiveTitleText,
+      QuestObjectivesTitleText
+    }
+
+    for _, text in pairs(texts) do
+      SetWhite(text)
+    end
+
+    SetWhite(QuestFrameNpcNameText)
+    SetWhite(GossipFrameNpcNameText)
+    SetGossipButtonsWhite()
+  end
+
+  do
     StripTextures(QuestGreetingScrollChildFrame)
 
     QuestTitleText:SetPoint("TOPLEFT", 10, -10)
@@ -26,6 +106,7 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
 
     hooksecurefunc("QuestFrameItems_Update", function()
       QuestRewardItemHighlight:Hide()
+      SetQuestTextWhite()
     end)
 
     hooksecurefunc("QuestRewardItem_OnClick", function()
@@ -34,6 +115,74 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
         QuestRewardItemHighlight:Show()
       end
     end)
+
+    if QuestFrame_SetTextColor then
+      hooksecurefunc("QuestFrame_SetTextColor", function(text)
+        SetWhite(text)
+      end)
+    end
+
+    if QuestFrame_SetTitleTextColor then
+      hooksecurefunc("QuestFrame_SetTitleTextColor", function(text)
+        SetWhite(text)
+      end)
+    end
+
+    if QuestInfo_Display then
+      hooksecurefunc("QuestInfo_Display", function()
+        SetQuestTextWhite()
+      end)
+    end
+
+    if GossipFrameUpdate then
+      hooksecurefunc("GossipFrameUpdate", function()
+        SetQuestTextWhite()
+        SetGossipButtonsWhite()
+      end)
+    end
+
+    if GossipFrameAvailableQuestsUpdate then
+      hooksecurefunc("GossipFrameAvailableQuestsUpdate", function()
+        SetGossipButtonsWhite()
+      end)
+    end
+
+    if GossipFrameActiveQuestsUpdate then
+      hooksecurefunc("GossipFrameActiveQuestsUpdate", function()
+        SetGossipButtonsWhite()
+      end)
+    end
+
+    if GossipFrameOptionsUpdate then
+      hooksecurefunc("GossipFrameOptionsUpdate", function()
+        SetGossipButtonsWhite()
+      end)
+    end
+
+    if QuestFrameGreetingPanel_OnShow then
+      hooksecurefunc("QuestFrameGreetingPanel_OnShow", function()
+        SetQuestTextWhite()
+        SetGossipButtonsWhite()
+      end)
+    end
+
+    if QuestFrameDetailPanel_OnShow then
+      hooksecurefunc("QuestFrameDetailPanel_OnShow", function()
+        SetQuestTextWhite()
+      end)
+    end
+
+    if QuestFrameProgressPanel_OnShow then
+      hooksecurefunc("QuestFrameProgressPanel_OnShow", function()
+        SetQuestTextWhite()
+      end)
+    end
+
+    if QuestFrameRewardPanel_OnShow then
+      hooksecurefunc("QuestFrameRewardPanel_OnShow", function()
+        SetQuestTextWhite()
+      end)
+    end
 
     for _, name in pairs({ "QuestProgressItem", "QuestDetailItem", "QuestRewardItem" }) do
       for i = 1, 6 do
@@ -65,6 +214,8 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
 
         title:SetParent(item.backdrop)
         title:SetDrawLayer("OVERLAY")
+
+        SetWhite(title)
       end
     end
   end
@@ -73,6 +224,7 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
     local frameName = f
     local frame = _G[frameName.."Frame"]
     local NPCName = _G[frame:GetName().."NpcNameText"]
+
     CreateBackdrop(frame, nil, nil, .75)
     CreateBackdropShadow(frame)
 
@@ -88,6 +240,8 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
     NPCName:ClearAllPoints()
     NPCName:SetPoint("TOP", frame.backdrop, "TOP", 0, -10)
 
+    SetWhite(NPCName)
+
     for _, v in pairs(panels) do
       local panel = v
       if frameName == 'Gossip' and panel ~= 'Greeting' then break end
@@ -102,23 +256,21 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
 
       local bg = scroll:CreateTexture(nil, "LOW")
       bg:SetAllPoints()
-      bg:SetTexCoord(.1,1,0,1)
-      bg:SetTexture("Interface\\Stationery\\StationeryTest1")
+      bg:SetTexture(0, 0, 0, 0.75)
 
-      -- assign material backgrounds to the default one
       _G[fname.."MaterialTopLeft"].SetTexture = function(self, texture)
-        bg:SetTexture(texture)
+        bg:SetTexture(0, 0, 0, 0.75)
       end
 
       _G[fname.."MaterialTopLeft"].Hide = function()
-        bg:SetTexture("Interface\\Stationery\\StationeryTest1")
+        bg:SetTexture(0, 0, 0, 0.75)
       end
 
-      -- disable meterial backgrounds
       _G[fname.."MaterialTopLeft"].Show = function() return end
       _G[fname.."MaterialTopRight"].Show = function() return end
       _G[fname.."MaterialBotLeft"].Show = function() return end
       _G[fname.."MaterialBotRight"].Show = function() return end
+
       _G[fname.."MaterialTopLeft"]:Hide()
       _G[fname.."MaterialTopRight"]:Hide()
       _G[fname.."MaterialBotLeft"]:Hide()
@@ -126,14 +278,17 @@ pfUI:RegisterSkin("Gossip and Quest", function ()
 
       if panel ~= 'Greeting' then
         local num_items, hook_func
+
         if panel == 'Progress' then
           num_items = MAX_REQUIRED_ITEMS
           hook_func = "QuestFrameProgressItems_Update"
-          else
+        else
           num_items = MAX_NUM_ITEMS
           hook_func = "QuestFrameItems_Update"
         end
       end
     end
   end
+
+  SetQuestTextWhite()
 end)
